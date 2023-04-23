8ffc658650e7f273026dc25d64b9638f7d97c63c
Ralph Schuler <ralph.schuler@nyphon.de>
2023-04-24 01:12:39 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ErrorMapper } from "utils/ErrorMapper";
 declare global {
   /*
     Example types, expand on these or remove them and add your own.
-    Note: Values, properties defined here do no fully *exist* by this type definiton alone.
+    Note: Values, properties defined here do not fully *exist* by this type definiton alone.
           You must also give them an implemention if you would like to use them. (ex. actually setting a `role` property in a Creeps memory)
 
     Types added in this `global` block are in an ambient, global context. This is needed because `main.ts` is a module file (uses import or export).
